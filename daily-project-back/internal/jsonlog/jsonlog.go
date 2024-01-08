91dbbe75e1bc7717b78c3c924a315bed6e31d917
Marcelo <marcelo.guzman.g@usach.cl>
2024-01-08 19:18:21 -0300

    add custom logger with levels

---
+++ daily-project-back/internal/jsonlog/jsonlog.go
@@ -0,0 +1,102 @@
+package jsonlog
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+	"runtime/debug"
+	"sync"
+	"time"
+)
+
+type Level int8
+
+const (
+	LevelInfo Level = iota
+	LevelError
+	LevelFatal
+	LevelOff
+)
+
+// Return string representation for severity level.
+func (l Level) String() string {
+	switch l {
+	case LevelInfo:
+		return "INFO"
+	case LevelError:
+		return "ERROR"
+	case LevelFatal:
+		return "FATAL"
+	default:
+		return ""
+	}
+}
+
+type Logger struct {
+	out      io.Writer // output dest where logs will be written to
+	minLevel Level
+	mux      sync.Mutex // mutex for coordinating writes
+}
+
+func New(out io.WriteCloser, minlevel Level) *Logger {
+	return &Logger{
+		out:      out,
+		minLevel: minlevel,
+	}
+}
+
+func (l *Logger) PrintInfo(msg string, properties map[string]string) {
+	l.print(LevelInfo, msg, properties)
+}
+
+func (l *Logger) PrintError(err error, properties map[string]string) {
+	l.print(LevelFatal, err.Error(), properties)
+}
+
+func (l *Logger) PrintFatal(err error, properties map[string]string) {
+	l.print(LevelFatal, err.Error(), properties)
+	os.Exit(1)
+}
+
+func (l *Logger) print(level Level, message string, properties map[string]string) (int, error) {
+	// If the severity level of the log entry is below the minimum
+	// severity for the logger, then return with no further action.
+	if level < l.minLevel {
+		return 0, nil
+	}
+
+	aux := struct {
+		Level      string            `json:"level"`
+		Time       string            `json:"time"`
+		Message    string            `json:"message"`
+		Properties map[string]string `json:"properties,omitempty"`
+		Trace      string            `json:"trace,omitempty"`
+	}{
+		Level:      level.String(),
+		Time:       time.Now().UTC().Format(time.RFC3339),
+		Message:    message,
+		Properties: properties,
+	}
+
+	// Include a stack trace for entries at the ERROR and FATAL levels.
+	if level >= LevelError {
+		aux.Trace = string(debug.Stack())
+	}
+
+	var line []byte
+	line, err := json.Marshal(aux)
+	if err != nil {
+		line = []byte(LevelError.String() + ": unable tu marshal log message:" + err.Error())
+	}
+
+	// Lock the mutex so that log entries aren't intermingled in the
+	// output
+	l.mux.Lock()
+	defer l.mux.Unlock()
+
+	return l.out.Write(append(line, '\n'))
+}
+
+func (l *Logger) Write(message []byte) (n int, err error) {
+	return l.print(LevelError, string(message), nil)
+}
