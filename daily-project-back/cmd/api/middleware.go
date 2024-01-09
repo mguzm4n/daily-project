c2a30fd8904f90c0a23ceed31d9a0a6699fbbc74
Marcelo <marcelo.guzman.g@usach.cl>
2024-01-08 23:39:56 -0300

    update from global to ip-specific rate limiter

--- daily-project-back/cmd/api/middleware.go
+++ daily-project-back/cmd/api/middleware.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -23,16 +26,61 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	var (
+		mux     sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(1 * time.Minute)
+			mux.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) >= 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mux.Unlock()
+		}
+	}()
+
 	// Initialize new rate limiter.
 	reqsPerSecond := 2.0 // r
 	maxReqsOnBurst := 4  // b
-	limiter := rate.NewLimiter(rate.Limit(reqsPerSecond), maxReqsOnBurst)
 
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			if !limiter.Allow() {
-				app.rateLimitExceededResponse(w, r)
-				return
+			if app.config.limiter.enabled {
+				ip, _, err := net.SplitHostPort(r.RemoteAddr)
+				if err != nil {
+					app.serverErrorResponse(w, r, err)
+					return
+				}
+
+				// Prevent race conditions from here
+				mux.Lock()
+				if _, found := clients[ip]; !found {
+					clients[ip] = &client{
+						limiter: rate.NewLimiter(rate.Limit(reqsPerSecond), maxReqsOnBurst),
+					}
+				}
+				clients[ip].lastSeen = time.Now()
+
+				// Call Allow() on the current limiter for specific ip
+				if !clients[ip].limiter.Allow() {
+					mux.Unlock()
+					app.rateLimitExceededResponse(w, r)
+					return
+				}
+
+				// Unlock the mutex so clients map is writtable again.
+				// Do not use "defer" since that would mean mutex wouldn't be
+				// unlocked till all handlers returned.
+				mux.Unlock()
 			}
 
 			next.ServeHTTP(w, r)
